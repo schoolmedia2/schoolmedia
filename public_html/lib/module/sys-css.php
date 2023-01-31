1a4a8447fc4dc525670feae51c3d150946d689c5
schoolmedia2 <schoolmedia.tk@mail.ru>
2023-01-31 14:01:00 +0300

    new

--- public_html/lib/module/sys-css.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" type="text/css" href="/lib/css/anim.css?v1"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/dark.css?v7"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/main.css?v1"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/debug.css?v1"/>
-<link rel="stylesheet" type="text/css" href="/lib/css/scale.css?v1"/>
